5e1df2bd94e918dee97ca774c1511cedc234042f
Aliaksandr Talanau <aliaksandr_talanau@epam.com>
2018-04-20 12:46:17 +0300

    hw1
    Updated according to the comments from pull request

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import { resolve } from 'path';
 
 const app = express();
-
+app.use(express.static('client'));
 app.get('/', (req, res) => {
-    res.sendFile('d:/work/react-global-training/client/index.html');
+    res.sendFile(resolve('client', 'index.html'));
 });
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+const port = 8080;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
