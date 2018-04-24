be173124fa9d50177777fefa024486e4e88df4bc
Aliaksandr Talanau <aliaksandr_talanau@epam.com>
2018-04-24 15:31:45 +0300

    update babel config

--- config/webpack.config.babel.js
+++ config/webpack.config.babel.js
@@ -1,27 +1,3 @@
-import path from 'path';
+import config from './webpack.config';
 
-import module from './webpack.module';
-import plugins from './webpack.plugins';
-import resolve from './webpack.resolve';
-import devServer from './webpack.dev-server';
-
-export default (env, options) => {
-    const isProduction = options.mode === 'production';
-    return {
-        entry: {
-            app: './src/index.js',
-        },
-        output: {
-            path: path.resolve(__dirname, './dist'),
-            publicPath: 'http://localhost:3000/',
-            filename: '[name].bundle.js',
-            chunkFilename: '[name].bundle.js',
-        },
-        module,
-        plugins,
-        devServer,
-        resolve,
-        devtool: isProduction ? 'none' : 'source-map',
-        mode: isProduction ? 'production' : 'development',
-    };
-};
+export default config;
